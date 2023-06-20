c0dae17a5d872da612fd933c088744d1da5aa1db
adityamungse97 <adityamungse97@gmail.com>
2023-06-20 22:23:24 +0530

    port addded

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var express = require("express") //install express
 var app = express();
 var usersign = require('./usersign_in')
 const bodyParser = require("body-parser");
+require("dotenv").config();
+
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-var server = app.listen(process.env.PORT||3000,function(){
+var server = app.listen(process.env.PORT,function(){
     var port = server.address().port;
     console.log("Server is running on port "+port)
 
